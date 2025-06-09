f1f3bd446d10ddcfe28266c1455af984f7f12cb2
Probe-Konto <entwickl.konto@gmail.com>
2025-06-09 19:31:27 +0300

    second

--- script.js
+++ script.js
@@ -5,44 +5,29 @@ document.documentElement.classList.toggle('dark', !defaultTheme);
 document.querySelector('#themeBtn').addEventListener('click', () => document.documentElement.classList.toggle('dark'));
 
 ///////////////////////////////
-const todos = {
-    0: {
-        name: 'Check for GTA VI',
-        checked: true
-    },
+const todos = [
+    { task: "Clean the house", checked: false },
+    { task: 'Check for GTA VI', checked: false },
+    { task: 'Hunt mosquitoes', checked: false },
+    { task: "Read Nietzsche", checked: true },
+];
 
-    1: {
-        name: "Read Nietzsche",
-        checked: false
-    },
 
-    2: {
-        name: 'Hunt mosquitoes',
-        checked: false,
-    },
-
-    3: {
-        name: 'Clean the house',
-        checked: false
-    }
-}
-const todoArr = ['Check for GTA VI', "Read Nietzsche", 'Hunt mosquitoes', 'Clean the house'];
 const todoList = document.querySelector('#todoUl');
 const todoCount = document.querySelector('#todoCount');
 
 function updateTodoCount() {
-    todoCount.textContent = todoArr.length;
+    todoCount.textContent = todos.length;
 }
 
 
-function renderTodoList() {
+function renderTodoList(arr) {
     updateTodoCount();
-    todoArr.forEach((el, index) => {
+    arr.forEach((todo, index) => {
         todoList.insertAdjacentHTML('afterbegin',
-            `<li class="p-3 border-b-1 border-gray-400 dark:border-gray-600  ">
+            `<li class="p-3 border-b-1 border-gray-400 dark:border-gray-600 todo-item" draggable="false" >
                     <label class="flex items-center gap-4 ps-1 group">
-                        <!--hide default checkbox icon and work on a new custom one-->
-                        <input type="checkbox" class="hidden">
+                        <input type="checkbox" class="todoCheckbox hidden" ${todo.checked ? "checked" : ""} data-index=${index}>
 
                         <span class=" h-6 w-6 rounded-full border-2 border-gray-400 flex place-content-center p-1 
                             group-has-checked:border-0 group-has-checked:bg-gradient-to-br from-grad1 to-grad2">
@@ -50,7 +35,7 @@ function renderTodoList() {
                         </span>
                         <span
                             class="text-gray-500 dark:text-gray-300 group-has-checked:line-through group-has-checked:text-gray-400">
-                            ${el}
+                            ${todo.task}
                         </span>
 
                         <span class="hidden group-hover:block ms-auto ">
@@ -66,37 +51,85 @@ function renderTodoList() {
     })
 };
 
-renderTodoList();
+renderTodoList(todos);
 
 ////////////////////
-const newTodo = document.querySelector('#newTodo');
-
-newTodo.addEventListener('keydown', (e) => {
-
-    if (e.code === 'Enter' || e.code === 'NumpadEnter') {
-        todoList.replaceChildren();
-        todoArr.push(newTodo.value);
-        renderTodoList();
-        updateTodoCount();
-        newTodo.value = '';
-    }
-});
+const todoForm = document.querySelector('#todoForm');
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    todoList.replaceChildren();
+    todos.push({ task: e.target.querySelector('input').value, checked: false });
+    renderTodoList(todos);
+    updateTodoCount();
+    newTodo.value = '';
+})
 
 /////////////////////
 todoList.addEventListener('click', (e) => {
     if (e.target.classList.contains('deleteTodo')) {
         todoList.replaceChildren();
-        todoArr.splice(e.target.id, 1);
-        renderTodoList();
+        todos.splice(e.target.id, 1);
+        renderTodoList(todos);
         updateTodoCount();
     }
-})
-
-
+});
 
 /////////////////////
+const todoCheckboxes = document.querySelectorAll('.todoCheckbox');
+const all = document.querySelector('#all');
+const active = document.querySelector('#active');
+const completed = document.querySelector('#Completed');
+const clear = document.querySelector('#clear')
 
 
+todoList.addEventListener('click', (e) => {
+    if (e.target.classList.contains('todoCheckbox')) {
+        todos.at(e.target.dataset.index).checked = !todos.at(e.target.dataset.index).checked;
+    }
+});
+
+all.addEventListener('click', () => {
+    all.classList.add('activeBtn');
+    active.classList.remove('activeBtn');
+    completed.classList.remove('activeBtn');
+    todoList.replaceChildren();
+    renderTodoList(todos);
+});
+
+completed.addEventListener('click', () => {
+    completed.classList.add('activeBtn');
+    all.classList.remove('activeBtn');
+    active.classList.remove('activeBtn');
+
+    todoList.replaceChildren();
+    renderTodoList(todos.filter(todo => todo.checked === true))
+}
+);
 
 
+active.addEventListener('click', () => {
+    active.classList.add('activeBtn');
+    all.classList.remove('activeBtn');
+    completed.classList.remove('activeBtn');
+
+    todoList.replaceChildren();
+    renderTodoList(todos.filter(todo => todo.checked === false))
+}
+);
+
+clear.addEventListener('click', () => {
+    all.classList.add('activeBtn');
+    active.classList.remove('activeBtn');
+    completed.classList.remove('activeBtn');
+
+    todoList.replaceChildren();
+
+    for (let i = todos.length - 1; i >= 0; i--) {
+        if (todos[i].checked) {
+            todos.splice(i, 1);
+        }
+    }
+
+    renderTodoList(todos); // ← you missed this
+});
 
